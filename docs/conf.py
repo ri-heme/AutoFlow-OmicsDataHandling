5c9ab38179e2bdfa2ae2a3a79261404423f43999
ri-heme <72443078+ri-heme@users.noreply.github.com>
2021-03-23 11:48:16 +0100

    docs: add sphinx documentation
    First documentation commit including contributor guide, documentation of the pathways module, and skeleton for further sections.

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path("..").resolve()))
+
+# -- Project information -----------------------------------------------------
+
+project = 'BFAIR'
+copyright = '2021, AutoFlowResearch'
+author = 'AutoFlowResearch'
+
+# The full version, including alpha/beta/rc tags
+release = '0.0.1'
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_rtd_theme",
+    "nb2plots",
+    "numpydoc",
+]
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+autosummary_generate = True
+numpydoc_show_class_members = False
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/reference/index.rst
@@ -0,0 +1,9 @@
+API Reference
+=============
+
+.. toctree::
+   :maxdepth: 2
+
+   introduction
+   pathways/index
+   database_structure
