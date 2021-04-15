adc0ae0e1fb679759d83a921408b6b8fb2cb31e6
ri-heme <72443078+ri-heme@users.noreply.github.com>
2021-04-15 11:55:31 +0200

    test: fix mock test

--- tests/test_rdkit.py
+++ tests/test_rdkit.py
@@ -1,18 +1,19 @@
+import sys
 import unittest
-from unittest import mock
+from unittest.mock import Mock, patch
 from types import ModuleType
 
 
 class TestRdkit(unittest.TestCase):
-
+    @patch.dict(sys.modules, rdkit=None)
     def test_mock(self):
         # Halting correct import of RDKit should produce mocks instead of modules
-        with mock.patch.dict("sys.modules", rdkit=None):
-            from BFAIR.rdkit import Chem, AllChem, Descriptors, Draw
-            for mock_module in [Chem, AllChem, Descriptors, Draw]:
-                self.assertNotIsInstance(mock_module, ModuleType)
-                with self.assertRaises(ModuleNotFoundError):
-                    mock_module.test()
+        from BFAIR.rdkit import Chem, AllChem, Descriptors, Draw
+        for mock_module in [Chem, AllChem, Descriptors, Draw]:
+            self.assertNotIsInstance(mock_module, ModuleType)
+            self.assertIsInstance(mock_module.test, Mock)
+            with self.assertRaises(ModuleNotFoundError):
+                mock_module.test()
 
     def test_import(self):
         from BFAIR.rdkit import Chem, AllChem, Descriptors, Draw
